831c357cb72172766909409f0296e19811082504
Yasmin Souza <yasmin075souza@hotmail.com>
2023-10-27 10:58:57 -0300

    adicionando componentes ; instalando as rotas

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,27 @@
 import "./styles/normalize.css";
 import "./styles/fontawesome.min.css";
 import "./styles/main.css";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom"; //npm i react-router-dom
+
+import InicialPage from "./components/pages/InicialPage";
+import Users from "./components/pages/Users";
+import BlogUsers from "./components/pages/BlogUsers";
 import CreatePost from "./components/pages/CreatePost";
-// import InicialPage from "./components/pages/InicialPage";
-// import Users from "./components/pages/users";
-// import BlogUsers from "./components/pages/BlogUsers";
+
 
 function App() {
-  return <CreatePost />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<InicialPage />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/users/:userId" element={<BlogUsers />} />
+        <Route path="/users/:userId/post" element={<CreatePost />} />
+        <Route path="*" element={ <h1>PAGE NOT FOUND</h1> }/>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
