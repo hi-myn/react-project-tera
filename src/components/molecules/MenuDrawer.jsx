8e64aa369a477dfbf2c9ee12faaf4c97650c28d8
Yasmin Souza <yasmin075souza@hotmail.com>
2023-11-03 20:00:26 -0300

    menu de navegação

--- src/components/molecules/MenuDrawer.jsx
+++ src/components/molecules/MenuDrawer.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import Drawer from "@mui/material/Drawer";
+import { List, ListItem } from "@mui/material";
+import { Link } from "react-router-dom";
 
 export default function MenuDrawer(props) {
   return (
-    <Drawer anchor="right" open={props.open} onClose={() => props.setOpen(false)}>
-      Hello Everyone!!!!!!
+    <Drawer
+      anchor="right"
+      open={props.open}
+      onClose={() => props.setOpen(false)}
+    >
+      <List>
+        <ListItem>
+          <Link to="/">Home</Link>
+        </ListItem>
+        <ListItem>
+          <Link to="/users">Users</Link>
+        </ListItem>
+        <ListItem>
+          <Link to="/users/1/post">Criar Post</Link>{" "}
+        </ListItem>
+      </List>
     </Drawer>
   );
 }
--- src/components/templates/Default.jsx
+++ src/components/templates/Default.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import logo from "../../images/logo.svg";
 import MenuDrawer from "../molecules/MenuDrawer";
+import { Link } from "react-router-dom";
 
 export default function Default(props) {
   //props é o objeto que contém todas as props que são passadas na instanciação do componente
-  const [ open, setOpen ] = React.useState(false);
+  const [open, setOpen] = React.useState(false);
 
   return (
     <div className="wrapper">
-      <MenuDrawer open={open} setOpen = {setOpen} />
+      <MenuDrawer open={open} setOpen={setOpen} />
       <div className="app-header">
         <div className="app-header__logo">
-          <img src={logo} className="responsive" alt="" />
+          <Link to="/">
+            <img src={logo} className="responsive" alt="" />
+          </Link>
         </div>
         <div className="app-header__menu">
           <i onClick={() => setOpen(true)} className="fa fa-bars"></i>
